b113341d98d2cd77ff76284db80c1ed35517ee3a
tina-kimhse <abas2000@naver.com>
2024-07-11 17:19:16 +0900

    client js 실습

---
+++ main.js
@@ -0,0 +1,21 @@
+//1. alwrt() : 알림창을 호출하는 명령
+
+//2. console.log() : (브라우저나 터미널에) 콘솔에 메세지를 출력하는 명령
+//코드를 실행할때 오류(bug)가 생기거나 예기치 않는 결과가 나올때 
+//2-1. 숫자
+//2-2. 문자 : "문자" or '문자' or '템플릿문자' :백틱/그레이브 악센트
+
+//APT or 내장객체(=빌트인, Built-in) 메소드 >>명령어
+//주석(comment) : 실행 코드를 설명으로 변경, 더이상 실행되지 않게 조치
+//한줄 주석
+/*여러줄주석
+alert(10);
+alert("Hello Js!");
+alert(10+5);
+*/
+
+//console.log()는 각자 써보세요(이전에도 써봤으니까!)
+//숫자, 문자 데이터 vs HTML 태그를 포함 출력
+document.write(10);
+document.write("<h1>Hello, Js!</h1>");
+document.write("<a href='https://www.naver.com'>네이버</a>");
